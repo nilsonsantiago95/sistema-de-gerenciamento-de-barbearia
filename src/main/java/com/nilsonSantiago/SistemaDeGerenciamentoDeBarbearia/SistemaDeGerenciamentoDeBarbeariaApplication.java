24f763bdf6e4bfb55da848b40df20ab0335d0590
Nilson Santiago <nilson.santiago19@gmail.com>
2026-02-25 02:14:52 -0300

    first commit

---
+++ src/main/java/com/nilsonSantiago/SistemaDeGerenciamentoDeBarbearia/SistemaDeGerenciamentoDeBarbeariaApplication.java
@@ -0,0 +1,13 @@
+package com.nilsonSantiago.SistemaDeGerenciamentoDeBarbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGerenciamentoDeBarbeariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGerenciamentoDeBarbeariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nilsonSantiago/SistemaDeGerenciamentoDeBarbearia/SistemaDeGerenciamentoDeBarbeariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilsonSantiago.SistemaDeGerenciamentoDeBarbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGerenciamentoDeBarbeariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
